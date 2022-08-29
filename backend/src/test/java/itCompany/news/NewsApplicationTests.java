6d92038884dabfa736b17c229b1b5d2cda49f469
Michael Babcock <babcockmichael17@gmail.com>
2022-08-29 12:08:19 -0700

    first commit

---
+++ backend/src/test/java/itCompany/news/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package itCompany.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
